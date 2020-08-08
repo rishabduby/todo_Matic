f59f758735aaf56fe34bdac9c60c5556fcdd9373
rishabduby <rishabduby@gmail.com>
2020-08-08 18:37:05 +0530

    Updates

---
+++ precache-manifest.1097be82ffc3ca822261b7d884ddba30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b4db100419a98843732b79b014e2421",
+    "url": "/todo_Matic/index.html"
+  },
+  {
+    "revision": "ce6b5976373ef0d89a29",
+    "url": "/todo_Matic/static/css/main.dad12839.chunk.css"
+  },
+  {
+    "revision": "cb74cd7ba9c4808c112f",
+    "url": "/todo_Matic/static/js/2.32417aed.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/todo_Matic/static/js/2.32417aed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce6b5976373ef0d89a29",
+    "url": "/todo_Matic/static/js/main.69cf64a5.chunk.js"
+  },
+  {
+    "revision": "044d78455f327e754338",
+    "url": "/todo_Matic/static/js/runtime-main.a959f9ba.js"
+  }
+]);
